c8c5f261bf089c12eeb534449a035e48451c7a43
ericweres <128173839+ericweres@users.noreply.github.com>
2023-05-15 20:25:58 +0200

    feat: added tests

---
+++ __tests__/lied/lied-query.resolver.test.ts
@@ -0,0 +1,270 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-non-null-assertion, @typescript-eslint/no-extra-non-null-assertion */
+/*
+ * Copyright (C) 2021 - present Juergen Zimmermann, Hochschule Karlsruhe
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import { type GraphQLRequest, type GraphQLResponse } from 'apollo-server-types';
+import { afterAll, beforeAll, describe, expect, test } from '@jest/globals';
+import axios, { type AxiosInstance, type AxiosResponse } from 'axios';
+import {
+    host,
+    httpsAgent,
+    port,
+    shutdownServer,
+    startServer,
+} from '../testserver.js';
+import { type LiedDTO } from '../../src/lied/graphql/lied-query.resolver.js';
+import { HttpStatus } from '@nestjs/common';
+
+/* eslint-disable jest/no-export */
+export type GraphQLQuery = Pick<GraphQLRequest, 'query'>;
+export type GraphQLResponseBody = Pick<GraphQLResponse, 'data' | 'errors'>;
+/* eslint-enable jest/no-export */
+
+// -----------------------------------------------------------------------------
+// T e s t d a t e n
+// -----------------------------------------------------------------------------
+const idVorhanden = '1';
+
+const titelVorhanden = 'Alpha';
+
+const teilTitelVorhanden = 'a';
+
+const teilTitelNichtVorhanden = 'abc';
+
+// -----------------------------------------------------------------------------
+// T e s t s
+// -----------------------------------------------------------------------------
+// Test-Suite
+// eslint-disable-next-line max-lines-per-function
+describe('GraphQL Queries', () => {
+    let client: AxiosInstance;
+    const graphqlPath = 'graphql';
+
+    // Testserver starten und dabei mit der DB verbinden
+    beforeAll(async () => {
+        await startServer();
+        const baseURL = `https://${host}:${port}/`;
+        client = axios.create({
+            baseURL,
+            httpsAgent,
+        });
+    });
+
+    afterAll(async () => {
+        await shutdownServer();
+    });
+
+    test('Lied zu vorhandener ID', async () => {
+        // given
+        const body: GraphQLRequest = {
+            query: `
+                {
+                    lied(id: "${idVorhanden}") {
+                        titel
+                        version
+                        rating
+                        art
+                    }
+                }
+            `,
+        };
+
+        // when
+        const response: AxiosResponse<GraphQLResponseBody> = await client.post(
+            graphqlPath,
+            body,
+        );
+
+        // then
+        const { status, headers, data } = response;
+
+        expect(status).toBe(HttpStatus.OK);
+        expect(headers['content-type']).toMatch(/json/iu);
+        expect(data.errors).toBeUndefined();
+        expect(data.data).toBeDefined();
+
+        const { lied } = data.data!;
+        const result: LiedDTO = lied;
+
+        expect(result.titel).toMatch(/^\w/u);
+        expect(result.version).toBeGreaterThan(-1);
+        expect(result.id).toBeUndefined();
+    });
+
+    test('Lied zu nicht-vorhandener ID', async () => {
+        // given
+        const id = '999999';
+        const body: GraphQLRequest = {
+            query: `
+                {
+                    lied(id: "${id}") {
+                        titel 
+                    }
+                }
+            `,
+        };
+
+        // when
+        const response: AxiosResponse<GraphQLResponseBody> = await client.post(
+            graphqlPath,
+            body,
+        );
+
+        // then
+        const { status, headers, data } = response;
+
+        expect(status).toBe(HttpStatus.OK);
+        expect(headers['content-type']).toMatch(/json/iu);
+        expect(data.data!.lied).toBeNull();
+
+        const { errors } = data;
+
+        expect(errors).toHaveLength(1);
+
+        const [error] = errors!;
+        const { message, path, extensions } = error!;
+
+        expect(message).toBe(`Es wurde kein Lied mit der ID ${id} gefunden.`);
+        expect(path).toBeDefined();
+        expect(path!![0]).toBe('lied');
+        expect(extensions).toBeDefined();
+        expect(extensions!.code).toBe('BAD_USER_INPUT');
+    });
+
+    test('Lied zu vorhandenem Titel', async () => {
+        // given
+        const body: GraphQLRequest = {
+            query: `
+                {
+                    lieder(titel: "${titelVorhanden}") {
+                        art
+                        titel 
+                    }
+                }
+            `,
+        };
+
+        // when
+        const response: AxiosResponse<GraphQLResponseBody> = await client.post(
+            graphqlPath,
+            body,
+        );
+
+        // then
+        const { status, headers, data } = response;
+
+        expect(status).toBe(HttpStatus.OK);
+        expect(headers['content-type']).toMatch(/json/iu);
+        expect(data.errors).toBeUndefined();
+
+        expect(data.data).toBeDefined();
+
+        const { lieder } = data.data!;
+
+        expect(lieder).not.toHaveLength(0);
+
+        const liederArray: LiedDTO[] = lieder;
+
+        expect(liederArray).toHaveLength(1);
+
+        const [lied] = liederArray;
+
+        expect(lied!.titel).toBe(titelVorhanden);
+    });
+
+    test('Lied zu vorhandenem Teil-Titel', async () => {
+        // given
+        const body: GraphQLRequest = {
+            query: `
+                {
+                    lieder(titel: "${teilTitelVorhanden}") {
+                        art
+                        titel
+                    }
+                }
+            `,
+        };
+
+        // when
+        const response: AxiosResponse<GraphQLResponseBody> = await client.post(
+            graphqlPath,
+            body,
+        );
+
+        // then
+        const { status, headers, data } = response;
+
+        expect(status).toBe(HttpStatus.OK);
+        expect(headers['content-type']).toMatch(/json/iu);
+        expect(data.errors).toBeUndefined();
+        expect(data.data).toBeDefined();
+
+        const { lieder } = data.data!;
+
+        expect(lieder).not.toHaveLength(0);
+
+        const liederArray: LiedDTO[] = lieder;
+        liederArray
+            .map((lied) => lied.titel)
+            .forEach((titel) =>
+                expect(titel?.toLowerCase()).toEqual(
+                    expect.stringContaining(teilTitelVorhanden),
+                ),
+            );
+    });
+
+    test('Lied zu nicht vorhandenem Titel', async () => {
+        // given
+        const body: GraphQLRequest = {
+            query: `
+                {
+                    lieder(titel: "${teilTitelNichtVorhanden}") {
+                        art
+                        titel
+                    }
+                }
+            `,
+        };
+
+        // when
+        const response: AxiosResponse<GraphQLResponseBody> = await client.post(
+            graphqlPath,
+            body,
+        );
+
+        // then
+        const { status, headers, data } = response;
+
+        expect(status).toBe(HttpStatus.OK);
+        expect(headers['content-type']).toMatch(/json/iu);
+        expect(data.data!.lieder).toBeNull();
+
+        const { errors } = data;
+
+        expect(errors).toHaveLength(1);
+
+        const [error] = errors!;
+        const { message, path, extensions } = error!;
+
+        expect(message).toBe('Es wurden keine Lieder gefunden.');
+        expect(path).toBeDefined();
+        expect(path!![0]).toBe('lieder');
+        expect(extensions).toBeDefined();
+        expect(extensions!.code).toBe('BAD_USER_INPUT');
+    });
+});
+/* eslint-enable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-non-null-assertion, @typescript-eslint/no-extra-non-null-assertion */
